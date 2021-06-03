1f5f277b2b042d0cb088be67a1d540c7804b3a9a
Ki-van <kirill.ivanin00@yandex.ru>
2021-06-03 23:45:06 +0300

    Add project files.

---
+++ Optimize.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB_3_Coordinate_Descent
+{
+    class Optimize
+    {
+        static double e = 0.01;
+        static double dx = 0.05;
+
+        public delegate double dfunction(double[] vars);
+        static dfunction func;
+       public static double[] minimize(dfunction function, params double[] vars)
+        {
+            func = function;
+            double min = function(vars);
+            bool minf = false;
+
+            while (!minf)
+            {
+                for (
+                    int i = 0; i < vars.Length; i++)
+                {
+                    if((i == 0 || minf == true) && dFunc(vars, i) + e >= 0)
+                    {
+                        minf = true;
+                        continue;
+                    }
+                   
+                    while (dFunc(vars, i) + e < 0)
+                    {
+                        vars[i] += dx;
+                       
+                    }
+                    
+                }
+            }
+            return vars;
+        }
+
+        static double dFunc(double[] vars, int i)
+        {
+            double[] dVars = new double[vars.Length];
+            Array.Copy(vars, dVars, vars.Length);
+            dVars[i] += dx;
+            return (func(dVars) - func(vars)) / dx;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LB_3_Coordinate_Descent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] min = Optimize.minimize(func, 0, 0);
+            for (int i = 0; i < min.Length; i++)
+            {
+                Console.WriteLine("x{0} = {1}", i + 1, min[i]);
+            }
+            Console.WriteLine("F min = {0}", func(min));
+        }
+
+        static public double func(double[] vars)
+        {
+            return Math.Pow(vars[0], 4)
+                + Math.Pow(vars[1], 4)
+                + 2 * Math.Pow(vars[0], 2) * Math.Pow(vars[1], 2)
+                + -4 * vars[0] + 3;
+
+        }
+    }
+}
